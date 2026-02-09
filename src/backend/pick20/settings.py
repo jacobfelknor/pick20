4294814380c695defbdc0487d67525592ad04ada
Jacob Felknor <jacobfelknor073@gmail.com>
2026-02-09 11:30:37 -0700

    reduce jwt access token lifetime

--- src/backend/pick20/settings.py
+++ src/backend/pick20/settings.py
@@ -63,7 +63,7 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",)}
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
