1c51fed93c90d1b1f0ef4b7e031429b33b538139
Jacob Felknor <jacobfelknor073@gmail.com>
2026-02-12 15:56:03 +0000

    split user views and serializers

---
+++ src/backend/accounts/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- src/backend/accounts/views.py
+++ src/backend/accounts/views.py
@@ -1,23 +1,14 @@
-from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
-
+from .serializers import UserSerializer
 from rest_framework import generics
-from rest_framework.serializers import ModelSerializer
 
+from django.contrib.auth import get_user_model
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+User = get_user_model()
 
 
 class CreateUserView(generics.CreateAPIView):
