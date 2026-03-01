5436aaf00edb47f3249063b7e5f85d27ea64a0e2
Jacob Felknor <jacobfelknor073@gmail.com>
2026-03-01 02:25:44 +0000

    show alert if tournament hasn't started for a limited entries view

--- src/frontend/src/pages/Entries.tsx
+++ src/frontend/src/pages/Entries.tsx
@@ -1,8 +1,11 @@
-import { Title, Stack, Text } from "@mantine/core";
+import { Title, Stack, Alert } from "@mantine/core";
 import { useOutletContext } from "react-router-dom";
 import EntryTable from "../tables/EntryTable";
 import { useQuery } from "@tanstack/react-query";
 import api from "../api";
+import dayjs from 'dayjs';
+import { IconInfoCircle } from "@tabler/icons-react";
+import { useMemo } from "react";
 
 function Entries() {
     // context passed from appshell outlet
@@ -14,11 +17,17 @@ function Entries() {
         enabled: !!tournament,
     });
 
+    const startDateStr = useMemo(() => `at ${dayjs(tournamentDetail?.start_date).format("h:mm A")} on ${dayjs(tournamentDetail?.start_date).format("MMMM D, YYYY")}`, [tournamentDetail])
+
     return (
         <Stack>
             <Title order={2}>Entries for the {isTournamentDetailLoading ? '...' : tournamentDetail?.year} Tournament</Title>
 
-            {(tournamentDetail && !tournamentDetail?.is_locked) && <Text>TOURNAMENT HASN'T STARTED! You may only see your own entries</Text>}
+            {(tournamentDetail && !tournamentDetail?.is_locked) &&
+                <Alert variant="light" color="yellow" title="Tournament Hasn't Started!" icon={<IconInfoCircle />}>
+                    Until the tournament starts <b>{startDateStr}</b>, you can only see your own entries.
+                </Alert>
+            }
 
             <EntryTable tournament={tournament} />
 
