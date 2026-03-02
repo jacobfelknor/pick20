06db16aa5a5e3016c7a5fed873b68d994df061f4
Jacob Felknor <jacobfelknor073@gmail.com>
2026-03-01 20:26:37 -0700

    more prod settings

--- src/backend/pick20/prod_settings.py
+++ src/backend/pick20/prod_settings.py
@@ -7,3 +7,11 @@ ALLOWED_HOSTS = ["pick20.jacobfelknor.com"]
 CORS_ALLOWED_ORIGINS = [
     "https://pick20.jacobfelknor.com",
 ]
+
+# The domain where your site is actually hosted
+CSRF_TRUSTED_ORIGINS = [
+    "https://pick20.jacobfelknor.com",
+]
+
+# This tells Django to trust the 'X-Forwarded-Proto' header from NGINX
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
