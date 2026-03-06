fd272f588dede3b47c540c94399955b49cb096f2
Jacob Felknor <jacobfelknor073@gmail.com>
2026-03-06 04:37:39 +0000

    trust localhost in prod to enable frontend dev against prod api

--- src/backend/pick20/prod_settings.py
+++ src/backend/pick20/prod_settings.py
@@ -6,12 +6,14 @@ ALLOWED_HOSTS = ["pick20.jacobfelknor.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://pick20.jacobfelknor.com",
+    "http://localhost:5173",
 ]
 
 # The domain where your site is actually hosted
 CSRF_TRUSTED_ORIGINS = [
     "https://pick20.jacobfelknor.com",
+    "http://localhost:5173",
 ]
 
 # This tells Django to trust the 'X-Forwarded-Proto' header from NGINX
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
