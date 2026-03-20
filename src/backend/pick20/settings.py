7f90ee4f9ac43cd8e84c16f599c945cee905982e
Jacob Felknor <jacobfelknor073@gmail.com>
2026-03-20 00:53:25 +0000

    extend sessions times

--- src/backend/pick20/settings.py
+++ src/backend/pick20/settings.py
@@ -68,8 +68,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=21),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
